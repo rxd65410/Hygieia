66cd6c2a1c0b3c58cc42bd765e37aa4bf860741b
RAVITEJA DUGGIRALA <dravi475@gmail.com>
2017-06-24 15:32:07 -0500

    pr request from ui

---
+++ api/src/main/java/com/capitalone/dashboard/request/PrRequest.java
@@ -0,0 +1,10 @@
+@NotNull
+    private ObjectId componentId;
+    private Integer numberOfDays;
+    private Long pullRequestDateBegins;
+    private Long pullRequestDateEnds;
+    private Long changesGreaterThan;
+    private Long changesLessThan;
+    private List<String> revisionNumbers = new ArrayList<>();
+    private List<String> authors = new ArrayList<>();
+    private String messageContains;
